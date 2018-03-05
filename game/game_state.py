7eeb67aa2b4023249e44f12d81fd9f7169d0d4e8
Serillan <serillan11@gmail.com>
2018-03-05 02:21:24 +0100

    Change GameState's earth map to general planet map.

--- game/game_state.py
+++ game/game_state.py
@@ -5,18 +5,18 @@ import battlecode as bc
 class GC:
 
     # TODO: rework this
-    def __init__(self, game_state):
+    def __init__(self, game_state: bc.GameController):
         if GC._game_state is not None:
             # TODO: throw better exception
             raise Exception("State already set")
 
         GC._game_state = game_state
         GC._karbonite_deposits = KarboniteDeposits(game_state)
-        GC._earth_map = game_state.starting_map(bc.Planet.Earth)
+        GC._planet_map = game_state.starting_map(game_state.planet())
 
     _game_state = None
     _karbonite_deposits = None
-    _earth_map = None
+    _planet_map = None
 
     @staticmethod
     def get() -> bc.GameController:
@@ -28,5 +28,5 @@ class GC:
         return GC._karbonite_deposits.get_nearest(location)
 
     @staticmethod
-    def get_earth_map() -> bc.PlanetMap:
-        return GC._earth_map
+    def get_planet_map() -> bc.PlanetMap:
+        return GC._planet_map
