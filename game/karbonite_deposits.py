de6ba0c4b6bd85c231bb1448648f15fcbb9f515e
Serillan <serillan11@gmail.com>
2018-03-05 17:38:47 +0100

    KarboniteDeposits minor update.

--- game/karbonite_deposits.py
+++ game/karbonite_deposits.py
@@ -1,10 +1,20 @@
 import battlecode as bc
 
+class KarboniteDepositInfo:
+
+    def __init__(self, location, initial_karbonite):
+        self.location = location
+        self.initial_karbonite = initial_karbonite
+        self.observed_karbonite = initial_karbonite
+        self.observed_owned_by_enemy = False
+        self.being_harvested = False
+
 
 class KarboniteDeposits:
 
+    _deposits: [KarboniteDepositInfo] = []
+
     def __init__(self, game_controller):
-        self._deposits = []
         planet_map = game_controller.starting_map(bc.Planet.Earth)
         center_location = bc.MapLocation(bc.Planet.Earth, int(round(planet_map.width / 2)), int(round(planet_map.height / 2)))
         all_map_locations = game_controller.all_locations_within(center_location, max(planet_map.width ** 2, planet_map.height ** 2))
@@ -14,7 +24,7 @@ class KarboniteDeposits:
             if location_karbonite_count > 0:
                 self._deposits.append(KarboniteDepositInfo(map_location, location_karbonite_count))
 
-    def get_nearest(self, location):
+    def get_nearest(self, location) -> KarboniteDepositInfo:
         # TODO: use pathfinder here (iterate over all deposits, get nearest paths to them and choose the one with minimal nearest path
         # TODO: but watch out for owned by enemy and also those that are running out!
         # TODO: watch out also to not len all workers go to the same deposits!
@@ -27,6 +37,7 @@ class KarboniteDeposits:
                 deposit_distance = abs(deposit.location.x - location.x) + abs(deposit.location.y - location.y)
                 if deposit_distance < min_distance:
                     nearest_deposit = deposit
+                    min_distance = deposit_distance
 
         print("DC: " + str(deposits_count))
         return nearest_deposit
@@ -36,11 +47,4 @@ class KarboniteDeposits:
             print("K Deposit: " + str(info.location.x) + ":" + str(info.location.y) + " - " + str(info.initial_karbonite) + ", " + str(info.observed_owned_by_enemy))
 
 
-class KarboniteDepositInfo:
 
-    def __init__(self, location, initial_karbonite):
-        self.location = location
-        self.initial_karbonite = initial_karbonite
-        self.observed_karbonite = initial_karbonite
-        self.observed_owned_by_enemy = False
-        self.being_harvested = False
