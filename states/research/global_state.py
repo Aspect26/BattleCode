522e36a4ac7075c6a027fb33c1f05709c7326140
Julius Flimmel <flimmelj@gmail.com>
2018-03-06 17:08:03 +0100

    Updated researcher
    We no longer use custom variable for the research queue, but instead use directly the research queue from battlecode

--- states/research/global_state.py
+++ states/research/global_state.py
@@ -1,7 +1,11 @@
+from typing import List
+
 import battlecode as bc
 from entities.team import Team
 from game.game_controller import GC
 from messages.block_karbonite_usage import BlockKarboniteMessage
+from messages.message import Message
+from messages.queue_research_message import QueueResearchMessage
 from messages.unblock_karbonite_usage import UnblockKarboniteMessage
 from states.state import State
 
@@ -9,8 +13,9 @@ from states.state import State
 class GlobalResearcherState(State):
     """
     Very simple researching. When we hit some fixed round number, we start some research.
+    This does not support planning two researches on the same time, but we shouldn't do that because it would use too
+    many resources at one time or stop units production for long time.
     """
-    # TODO: accept messages that require some research
 
     def __init__(self, entity):
         super().__init__(entity)
@@ -18,38 +23,55 @@ class GlobalResearcherState(State):
             10: bc.UnitType.Ranger,
             500: bc.UnitType.Rocket
         }
-        self._research_queue = []
         self._blocking_spending_karbonite = False
 
     def run(self) -> None:
         self._check_research_timeline()
-        if self._has_queued_research():
-            if self._has_enough_karbonite_for_next_research():
-                self._queue_next_research()
-            else:
+        if GC.get().research_info().has_next_in_queue():
+            if not self._has_enough_karbonite_for_next_research():
                 self._start_blocking_karbonite()
         elif self._blocking_spending_karbonite:
             self._stop_blocking_karbonite()
 
+    def process_message(self, message: Message) -> bool:
+        if message is QueueResearchMessage:
+            casted_message: QueueResearchMessage = message
+            self._enqueue_research_to_level(casted_message.unit_type, casted_message.level)
+            return True
+
+        return False
+
     def _check_research_timeline(self):
         current_round = GC.get().round()
         if current_round in self._research_timeline:
-            self._research_queue.append(self._research_timeline[current_round])
+            self._enqueue_research(self._research_timeline[current_round])
 
-    def _has_queued_research(self) -> bool:
-        return len(self._research_queue) > 0
+    def _enqueue_research_to_level(self, unit_type: bc.UnitType, level: int):
+        current_level = self._get_current_research_level(unit_type)
+        missing_levels = level - current_level
+        if missing_levels <= 0:
+            return
+        else:
+            self._enqueue_researches([unit_type] * missing_levels)
+
+    def _enqueue_researches(self, unit_type: List[bc.UnitType]) -> None:
+        self._research_queue.extend(unit_type)
+
+    def _enqueue_research(self, unit_type: bc.UnitType):
+        GC.get().queue_research(unit_type)
 
     def _has_enough_karbonite_for_next_research(self) -> bool:
         research_cost = self._get_next_research_cost()
         current_karbonite = GC.get().karbonite()
         return research_cost < current_karbonite
 
-    def _queue_next_research(self):
-        next_research = self._research_queue[0]
-        if not GC.get().queue_research(next_research):
-            print("ERROR: Could not research {0}".format(next_research))
-        else:
-            self._research_queue = self._research_queue[:-1]
+    def _get_next_research_cost(self) -> int:
+        research_unit = GC.get().research_info().next_in_queue()
+        level_to_research = GC.get().research_info().get_level(research_unit) + 1
+        return bc.cost_of(research_unit, level_to_research)
+
+    def _get_current_research_level(self, unit_type: bc.UnitType) -> int:
+        return GC.get().research_info().get_level(unit_type)
 
     def _start_blocking_karbonite(self):
         Team.instance.send_message_to_factories(BlockKarboniteMessage())
@@ -58,9 +80,3 @@ class GlobalResearcherState(State):
     def _stop_blocking_karbonite(self):
         Team.instance.send_message_to_factories(UnblockKarboniteMessage())
         self._blocking_spending_karbonite = False
-
-    def _get_next_research_cost(self) -> int:
-        research_unit = self._research_queue[0]
-        level_to_research = GC.get().research_info().get_level() + 1
-        return bc.cost_of(research_unit, level_to_research)
-
